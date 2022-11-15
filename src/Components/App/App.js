9e68ba5f90e6525b08302cc243a8ccd545defc05
Nathan <nathanbark4@gmail.com>
2022-11-15 14:27:30 +0200

    input button layout and basic styling

---
+++ src/Components/App/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import {InputButtons} from "../InputButtons/InputButtons.js"
+import {MainScreen} from '../MainScreen/MainScreen.js'
+
+function App() {
+  return (
+    <div>
+      {/* <MainScreen /> */}
+      <InputButtons />
+      {/* <FunctionButtons /> */}
+    </div>
+  )
+}
+
+export default App;
---
+++ src/Components/InputButtons/InputButtons.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './InputButtons.css'
+
+export const InputButtons = () => {
+  return (
+    <div className="input-btn-container">
+      <button className="input-btn" value="1">1</button>
+      <button className="input-btn" value="2">2</button>
+      <button className="input-btn" value="3">3</button>
+      <button className="input-btn" value="4">4</button>
+      <button className="input-btn" value="5">5</button>
+      <button className="input-btn" value="6">6</button>
+      <button className="input-btn" value="7">7</button>
+      <button className="input-btn" value="8">8</button>
+      <button className="input-btn" value="9">9</button>
+      <button className="input-btn btn-0" value="0">0</button>
+      <button className="input-btn" value=".">.</button>
+    </div>
+  );
+};
